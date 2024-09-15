0f923b4175ff715dce6186a430f8dc3f921c33a8
Pooja Parmar <pooja.par92@gmail.com>
2024-09-15 10:19:50 +0000

    model data set up finished

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-poojapar-satvikfood-854qz4fn8i4.ws-eu116.gitpod.io", ".herokuapp.com"]
 
--- satvik/models.py
+++ satvik/models.py
@@ -4,18 +4,22 @@ from django.contrib.auth.models import User
 # Model goes here 
 
 class Table(models.Model):
-    table_number = models.IntegerField(unique=True)
-    capacity = models.IntegerField()
+    table_number = models.IntegerField(choices=STATUS, default=0)  # Corresponds to HTML number input
+    capacity = models.IntegerField(choices=STATUS, default=0)  # Corresponds to HTML number input
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)   
 
     def __str__(self):
         return f'Table {self.table_number} - Capacity: {self.capacity}'
 
+
 class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
-    date = models.DateField()
-    time = models.TimeField()
-    guest_count = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Corresponds to HTML select dropdown
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)  # Corresponds to HTML select dropdown
+    date = models.DateField(auto_now_add=True)  # Corresponds to HTML date input
+    time = models.TimeField(auto_now_add=True)  # Corresponds to HTML time input
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Reservation for {self.user.username} on {self.date} at {self.time}'
